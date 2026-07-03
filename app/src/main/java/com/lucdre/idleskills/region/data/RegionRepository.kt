42399d8a1406c26d80d7d91c7feb4250b03f75b4
Lucdre <lucadanielrex95@gmail.com>
2026-07-03 21:21:23 +0200

    Removed the need to have RegionConfig as that data is in TrainingMethodRepositories.

--- app/src/main/java/com/lucdre/idleskills/region/data/RegionRepository.kt
+++ app/src/main/java/com/lucdre/idleskills/region/data/RegionRepository.kt
@@ -1,18 +1,31 @@
 package com.lucdre.idleskills.region.data
 
 import com.lucdre.idleskills.region.domain.Region
-import com.lucdre.idleskills.region.domain.RegionConfig
 import com.lucdre.idleskills.region.domain.RegionRepositoryInterface
 import com.lucdre.idleskills.skills.domain.skill.SkillType
+import com.lucdre.idleskills.skills.domain.training.TrainingMethodRepositoryInterface
 import javax.inject.Inject
 import javax.inject.Singleton
 
 /**
- * Region config repo.
+ * Repository responsible for handling region-specific logic and data.
+
+ * @property trainingMethodRepository The dispatcher used to query available training methods.
  */
 @Singleton
-class RegionRepository @Inject constructor() : RegionRepositoryInterface {
+class RegionRepository @Inject constructor(
+    private val trainingMethodRepository: TrainingMethodRepositoryInterface
+) : RegionRepositoryInterface {
+    
+    /**
+     * Retrieves all skills that have at least one training method available in the given region.
+     *
+     * @param region The region to check for available skills.
+     * @return A list of [SkillType]s present in the region.
+     */
     override fun getSkillsForRegion(region: Region): List<SkillType> {
-        return RegionConfig.getSkillsForRegion(region)
+        return SkillType.entries.filter { skillType ->
+            trainingMethodRepository.getTrainingMethodsForSkill(skillType, region).isNotEmpty()
+        }
     }
 }
